10dbd3ae88fe4be9e623e676250e57603db9c196
Xodarap7 <basyar86@gmail.com>
2022-01-19 14:31:55 +0500

    task 5th after code-review

--- brain_games/task5_code.py
+++ brain_games/task5_code.py
@@ -1,24 +1,28 @@
 from random import randint
+
 import prompt
+ ""
+MIN_NUM = 1
+MAX_NUM = 1000
+SUCCESS_COUNT = 0
 
 
 def even_or_not():
-    success_count = 0
     while success_count < 3:
 
-        number = randint(0, 1000)
+        number = randint(MIN_NUM, MAX_NUM)
         print('Answer "yes" if the number is even, otherwise answer "no".')
         print(f'Question: {number}')
         even_result = 'yes' if number % 2 == 0 else 'no'
 
         validation = False
-        while validation is False:
+        while not validation:
             user_resolution = prompt.string('Your answer: ')
             validation = check_validation(user_resolution)
-            if validation is None:
+            if not validation:
                 print('You can send "yes" or "no"')
         result = compare_result(user_resolution, even_result)
-        if result is True:
+        if result:
             success_count += 1
         else:
             success_count = 0
